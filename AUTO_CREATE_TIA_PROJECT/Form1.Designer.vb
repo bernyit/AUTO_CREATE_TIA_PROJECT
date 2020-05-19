f9fea5dfd34164b6fc202793b5a0d22f300e3696
Giovanni Bernardi <bernyit@gmail.com>
2020-05-19 22:57:04 +0200

    Create prime funzioni per Aprire TIA Aprire un progetto Prendere riferiemnto a un device prendere riferimento a un software salvare un progetto compilare un progetto creare un progetto con parametri opzionali leggere proprietà di un progetto

--- AUTO_CREATE_TIA_PROJECT/Form1.Designer.vb
+++ AUTO_CREATE_TIA_PROJECT/Form1.Designer.vb
@@ -22,10 +22,138 @@ Partial Class Form1
     'Non modificarla mediante l'editor del codice.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.btnOpenTia15 = New System.Windows.Forms.Button()
+        Me.btnCloseTia15 = New System.Windows.Forms.Button()
+        Me.btnOpenTia15Project = New System.Windows.Forms.Button()
+        Me.txt_Status = New System.Windows.Forms.TextBox()
+        Me.btnIterateDevices = New System.Windows.Forms.Button()
+        Me.btnCloseTia15Project = New System.Windows.Forms.Button()
+        Me.btnSaveTia15Project = New System.Windows.Forms.Button()
+        Me.btnCreateProject = New System.Windows.Forms.Button()
+        Me.btnProjectProperties = New System.Windows.Forms.Button()
+        Me.btnCompile = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'btnOpenTia15
+        '
+        Me.btnOpenTia15.Location = New System.Drawing.Point(58, 60)
+        Me.btnOpenTia15.Name = "btnOpenTia15"
+        Me.btnOpenTia15.Size = New System.Drawing.Size(203, 51)
+        Me.btnOpenTia15.TabIndex = 0
+        Me.btnOpenTia15.Text = "Apri TIA 15"
+        Me.btnOpenTia15.UseVisualStyleBackColor = True
+        '
+        'btnCloseTia15
+        '
+        Me.btnCloseTia15.Location = New System.Drawing.Point(58, 140)
+        Me.btnCloseTia15.Name = "btnCloseTia15"
+        Me.btnCloseTia15.Size = New System.Drawing.Size(203, 51)
+        Me.btnCloseTia15.TabIndex = 1
+        Me.btnCloseTia15.Text = "Chiudi TIA 15"
+        Me.btnCloseTia15.UseVisualStyleBackColor = True
+        '
+        'btnOpenTia15Project
+        '
+        Me.btnOpenTia15Project.Location = New System.Drawing.Point(482, 60)
+        Me.btnOpenTia15Project.Name = "btnOpenTia15Project"
+        Me.btnOpenTia15Project.Size = New System.Drawing.Size(196, 60)
+        Me.btnOpenTia15Project.TabIndex = 2
+        Me.btnOpenTia15Project.Text = "Apri Progetto"
+        Me.btnOpenTia15Project.UseVisualStyleBackColor = True
+        '
+        'txt_Status
+        '
+        Me.txt_Status.BackColor = System.Drawing.SystemColors.Menu
+        Me.txt_Status.Dock = System.Windows.Forms.DockStyle.Bottom
+        Me.txt_Status.Location = New System.Drawing.Point(0, 547)
+        Me.txt_Status.Name = "txt_Status"
+        Me.txt_Status.Size = New System.Drawing.Size(1231, 20)
+        Me.txt_Status.TabIndex = 20
+        '
+        'btnIterateDevices
+        '
+        Me.btnIterateDevices.Location = New System.Drawing.Point(482, 135)
+        Me.btnIterateDevices.Name = "btnIterateDevices"
+        Me.btnIterateDevices.Size = New System.Drawing.Size(196, 60)
+        Me.btnIterateDevices.TabIndex = 21
+        Me.btnIterateDevices.Text = "Iterate devices"
+        Me.btnIterateDevices.UseVisualStyleBackColor = True
+        '
+        'btnCloseTia15Project
+        '
+        Me.btnCloseTia15Project.Location = New System.Drawing.Point(482, 350)
+        Me.btnCloseTia15Project.Name = "btnCloseTia15Project"
+        Me.btnCloseTia15Project.Size = New System.Drawing.Size(196, 60)
+        Me.btnCloseTia15Project.TabIndex = 22
+        Me.btnCloseTia15Project.Text = "Chiudi Progetto"
+        Me.btnCloseTia15Project.UseVisualStyleBackColor = True
+        '
+        'btnSaveTia15Project
+        '
+        Me.btnSaveTia15Project.Location = New System.Drawing.Point(482, 284)
+        Me.btnSaveTia15Project.Name = "btnSaveTia15Project"
+        Me.btnSaveTia15Project.Size = New System.Drawing.Size(196, 60)
+        Me.btnSaveTia15Project.TabIndex = 23
+        Me.btnSaveTia15Project.Text = "Salva Progetto"
+        Me.btnSaveTia15Project.UseVisualStyleBackColor = True
+        '
+        'btnCreateProject
+        '
+        Me.btnCreateProject.Location = New System.Drawing.Point(717, 60)
+        Me.btnCreateProject.Name = "btnCreateProject"
+        Me.btnCreateProject.Size = New System.Drawing.Size(196, 60)
+        Me.btnCreateProject.TabIndex = 24
+        Me.btnCreateProject.Text = "Crea Progetto"
+        Me.btnCreateProject.UseVisualStyleBackColor = True
+        '
+        'btnProjectProperties
+        '
+        Me.btnProjectProperties.Location = New System.Drawing.Point(862, 232)
+        Me.btnProjectProperties.Name = "btnProjectProperties"
+        Me.btnProjectProperties.Size = New System.Drawing.Size(218, 83)
+        Me.btnProjectProperties.TabIndex = 25
+        Me.btnProjectProperties.Text = "Proprietà Progetto"
+        Me.btnProjectProperties.UseVisualStyleBackColor = True
+        '
+        'btnCompile
+        '
+        Me.btnCompile.Location = New System.Drawing.Point(482, 201)
+        Me.btnCompile.Name = "btnCompile"
+        Me.btnCompile.Size = New System.Drawing.Size(196, 60)
+        Me.btnCompile.TabIndex = 26
+        Me.btnCompile.Text = "Compila progetto"
+        Me.btnCompile.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.ClientSize = New System.Drawing.Size(1231, 567)
+        Me.Controls.Add(Me.btnCompile)
+        Me.Controls.Add(Me.btnProjectProperties)
+        Me.Controls.Add(Me.btnCreateProject)
+        Me.Controls.Add(Me.btnSaveTia15Project)
+        Me.Controls.Add(Me.btnCloseTia15Project)
+        Me.Controls.Add(Me.btnIterateDevices)
+        Me.Controls.Add(Me.txt_Status)
+        Me.Controls.Add(Me.btnOpenTia15Project)
+        Me.Controls.Add(Me.btnCloseTia15)
+        Me.Controls.Add(Me.btnOpenTia15)
+        Me.Name = "Form1"
         Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
     End Sub
 
+    Friend WithEvents btnOpenTia15 As Button
+    Friend WithEvents btnCloseTia15 As Button
+    Friend WithEvents btnOpenTia15Project As Button
+    Private WithEvents txt_Status As TextBox
+    Friend WithEvents btnIterateDevices As Button
+    Friend WithEvents btnCloseTia15Project As Button
+    Friend WithEvents btnSaveTia15Project As Button
+    Friend WithEvents btnCreateProject As Button
+    Friend WithEvents btnProjectProperties As Button
+    Friend WithEvents btnCompile As Button
 End Class
--- AUTO_CREATE_TIA_PROJECT/Form1.vb
+++ AUTO_CREATE_TIA_PROJECT/Form1.vb
@@ -1,3 +1,195 @@
-﻿Public Class Form1
+﻿Imports System.IO
+Imports Siemens.Engineering
+Imports Siemens.Engineering.Compiler
+Imports Siemens.Engineering.HW
+Imports Siemens.Engineering.HW.Features
+Imports Siemens.Engineering.SW
 
+Public Class Form1
+
+
+    Private MyTiaPortal As TiaPortal
+    Private MyProject As Project
+    Private MyDevice As Device
+    Private myPlcSoftware As PlcSoftware
+    Dim ProjectPath As String = ""
+
+    Private Sub btnOpenTia15_Click(sender As Object, e As EventArgs) Handles btnOpenTia15.Click
+        MyTiaPortal = New TiaPortal(TiaPortalMode.WithUserInterface)
+    End Sub
+
+    Private Sub btnCloseTia15_Click(sender As Object, e As EventArgs) Handles btnCloseTia15.Click
+        MyTiaPortal.Dispose()
+    End Sub
+
+    Private Sub btnOpenTia15Project_Click(sender As Object, e As EventArgs) Handles btnOpenTia15Project.Click
+        Dim fileSearch As OpenFileDialog = New OpenFileDialog
+
+        fileSearch.Filter = "*.ap15|*.ap15"
+        fileSearch.RestoreDirectory = True
+        fileSearch.ShowDialog()
+
+        ProjectPath = fileSearch.FileName.ToString()
+
+        If String.IsNullOrEmpty(ProjectPath) = False Then
+
+            Try
+
+                MyProject = MyTiaPortal.Projects.Open(New FileInfo(ProjectPath))
+                txt_Status.Text = "Project " + ProjectPath + " opened"
+
+            Catch ex As Exception
+                txt_Status.Text = "Error while opening project" + ex.Message
+
+            End Try
+        End If
+    End Sub
+
+    Private Sub btnCloseTia15Project_Click(sender As Object, e As EventArgs) Handles btnCloseTia15Project.Click
+        MyProject.Close()
+    End Sub
+
+    Private Sub btnIterateDevices_Click(sender As Object, e As EventArgs) Handles btnIterateDevices.Click
+
+        MsgBox(MyProject.Devices.Count)
+
+        If MyProject.Devices.Count > 0 Then
+
+            MyDevice = MyProject.Devices(0)
+            myPlcSoftware = GetPlcSoftware(MyDevice)
+        End If
+
+
+        'For Each device As Device In MyProject.Devices
+
+        '    Console.WriteLine(device.Name.ToString)
+        'Next
+
+    End Sub
+
+
+
+    ' 
+    ''' <summary>
+    ''' Pagina 165
+    ''' </summary>
+    ''' <param name="device"></param>
+    ''' <returns>Returns PlcSoftware</returns>
+    Private Function GetPlcSoftware(ByVal device As Device) As PlcSoftware
+        Dim deviceItemComposition As DeviceItemComposition = device.DeviceItems
+
+        For Each deviceItem As DeviceItem In deviceItemComposition
+            Dim softwareContainer As SoftwareContainer = deviceItem.GetService(Of SoftwareContainer)()
+
+            If softwareContainer IsNot Nothing Then
+                Dim softwareBase As Software = softwareContainer.Software
+                Dim plcSoftware As PlcSoftware = TryCast(softwareBase, PlcSoftware)
+                Return plcSoftware
+            End If
+        Next
+
+        Return Nothing
+    End Function
+
+
+
+    Private Sub btnSaveTia15Project_Click(sender As Object, e As EventArgs) Handles btnSaveTia15Project.Click
+        MyProject.Save()
+    End Sub
+
+    Private Sub btnCreateProject_Click(sender As Object, e As EventArgs) Handles btnCreateProject.Click
+        createProjectWithOptionalParameters()
+    End Sub
+
+    Private Sub createProjectWithStandardParameters()
+        Dim projectComposition As ProjectComposition = MyTiaPortal.Projects
+        Dim targetDirectory As DirectoryInfo = New DirectoryInfo("D:\TiaProjects")
+        ' Create a project with name MyProject
+        Dim project As Project = projectComposition.Create(targetDirectory, "MyProject")
+
+    End Sub
+
+    Private Sub createProjectWithOptionalParameters()
+        Dim projectComposition As ProjectComposition = MyTiaPortal.Projects
+        'Dim createParameters As New Dictionary(Of String, Object)
+
+        ' Create a project with both mandatory And optional parameters
+
+        'Pagina 102 del manuale, in C#... come si converte?
+        '(IEngineeringComposition)projectComposition).Create(TypeOf (Project), createParameters)
+        Dim createParameters As IEnumerable(Of KeyValuePair(Of String, Object)) = {
+            New KeyValuePair(Of String, Object)("TargetDirectory", New DirectoryInfo("D:\TiaProjects")),  ' Mandatory
+            New KeyValuePair(Of String, Object)("Name", "progettoCreatoAutomaticamente"),  ' Mandatory
+            New KeyValuePair(Of String, Object)("Author", "Giovanni"), ' Optional 
+            New KeyValuePair(Of String, Object)("Comment", "This project was created with Openness ")} ' Optional 
+
+
+        CType(ProjectComposition, IEngineeringComposition).Create(GetType(Project), createParameters)
+
+
+    End Sub
+
+
+
+    ''' <summary>
+    ''' Pagina 114 del manuale
+    ''' </summary>
+    ''' <param name="sender"></param>
+    ''' <param name="e"></param>
+    Private Sub btnProjectProperties_Click(sender As Object, e As EventArgs) Handles btnProjectProperties.Click
+
+        If ReferenceEquals(MyProject, Nothing) = False Then
+            Dim author As String = MyProject.Author
+            Dim Name As String = MyProject.Name
+            Dim Path As String = MyProject.Path.ToString
+            Dim creationTime As DateTime = MyProject.CreationTime
+            Dim modificationTime As DateTime = MyProject.LastModified
+            Dim lastModifiedBy As String = MyProject.LastModifiedBy
+            Dim Version As String = MyProject.Version
+            'Dim comment As MultilingualText = MyProject.Comment
+            Dim copyright As String = MyProject.Copyright
+            Dim family As String = MyProject.Family
+            Dim Size As Int64 = MyProject.Size
+            'Dim LanguageSettings As LanguageSettings = Project.LanguageSettings
+
+            Dim testo As String = ""
+
+            testo &= "author " & author & vbCrLf
+            testo &= "Name " & Name & vbCrLf
+            testo &= "Path " & Path & vbCrLf
+            testo &= "creationTime " & creationTime & vbCrLf
+            testo &= "modificationTime " & modificationTime & vbCrLf
+            testo &= "lastModifiedBy " & lastModifiedBy & vbCrLf
+            testo &= "Version " & Version & vbCrLf
+            'testo &= "" & comment & vbCrLf
+            testo &= "copyright " & copyright & vbCrLf
+            testo &= "family " & family & vbCrLf
+            testo &= "Size " & Size & vbCrLf
+
+            MsgBox(testo)
+
+        Else
+            MsgBox("progetto non aperto")
+        End If
+
+
+
+
+    End Sub
+
+
+
+
+
+
+
+    Public Sub CompilePlcSoftware(plcSoftware As PlcSoftware)
+
+        Dim compileService As ICompilable = plcSoftware.GetService(Of ICompilable)
+        Dim result As CompilerResult = compileService.Compile()
+    End Sub
+
+    Private Sub btnCompile_Click(sender As Object, e As EventArgs) Handles btnCompile.Click
+        CompilePlcSoftware(myPlcSoftware)
+    End Sub
 End Class
